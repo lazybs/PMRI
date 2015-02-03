b0442de15ee40f6f1d58bbcc7e2fc0a1c87452df
foo <foo>
2015-02-03 14:25:31 +0100

    merge conflict foo

--- app/src/main/java/lazybs/pmri/MainActivity.java
+++ app/src/main/java/lazybs/pmri/MainActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
@@ -17,6 +19,13 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent (MainActivity.this,MapsActivity.class));
+            }
+        });
     }
 
 
